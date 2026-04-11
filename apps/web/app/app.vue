1e161edf1c3f541a06c68dd316b79ca0df9b81e3
rodrigokovalski <rodrigokvk@gmail.com>
2026-04-11 17:59:15 -0300

    chore: instala design system no projeto web

--- apps/web/app/app.vue
+++ apps/web/app/app.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+  import { Button } from '@clicksign/design-system'
+</script>
+
 <template>
   <h1>Front end</h1>
+  <Button variant="primary">Exemplo</Button>
 </template>
